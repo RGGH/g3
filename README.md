3cc584c4e9512f3dbb3fd57c5bb21b690bd4de48
RGGH <pisscompany@gmail.com>
2026-01-15 13:59:39 +0000

    add mcp

---
+++ README.md
@@ -0,0 +1,1 @@
+# Check out Gemini 3
--- main.py
+++ main.py
@@ -1,10 +1,48 @@
+import os
+import asyncio
+from datetime import datetime
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 from google import genai
 
 client = genai.Client()
 
-response = client.models.generate_content(
-    model="gemini-3-flash-preview",
-    contents="Explain how AI works in a few words",
+# Create server parameters for stdio connection
+server_params = StdioServerParameters(
+    command="npx",  # Executable
+    args=["-y", "@philschmid/weather-mcp"],  # MCP Server
+    env=None,  # Optional environment variables
 )
 
-print(response.text)
+# response = client.models.generate_content(
+    # model="gemini-3-flash-preview",
+    # contents="Explain how AI works in a few words",
+# )
+
+async def run():
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            # Prompt to get the weather for the current day in London.
+            prompt = f"What is the weather in London in {datetime.now().strftime('%Y-%m-%d')}?"
+
+            # Initialize the connection between client and server
+            await session.initialize()
+
+            # Send request to the model with MCP function declarations
+            response = await client.aio.models.generate_content(
+                model="gemini-3-flash-preview",
+                #model="gemini-2.5-flash",
+                contents=prompt,
+                config=genai.types.GenerateContentConfig(
+                    temperature=0,
+                    tools=[session],  # uses the session, will automatically call the tool
+                    # Uncomment if you **don't** want the SDK to automatically call the tool
+                    # automatic_function_calling=genai.types.AutomaticFunctionCallingConfig(
+                    #     disable=True
+                    # ),
+                ),
+            )
+            print(response.text)
+
+# Start the asyncio event loop and run the main function
+asyncio.run(run())
